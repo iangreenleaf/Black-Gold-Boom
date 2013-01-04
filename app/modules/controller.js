c165984cffa023db81efb5c8c086dc93b784fbf0
Luis Brandao <luisbrandao@gmail.com>
2013-01-04 21:45:29 +0000

    Correctly use api url as set in index.html (Eric)

--- app/modules/controller.js
+++ app/modules/controller.js
@@ -44,7 +44,7 @@ function(app, Backbone, State, UI ) {
         autoplay: false,
         preloadRadius: 5,
         startFrame: 27708,
-        url: 'http://alpha.zeega.org/api/projects/3678' // bgb test
+        url: localStorage.api + '/projects/3678' // bgb test
       });
 
       player.on("all", function(e, obj){ if(e!="media_timeupdate") console.log('    zeega player event:',e,obj);});
