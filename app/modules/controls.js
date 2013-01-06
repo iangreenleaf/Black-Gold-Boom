4fe1a4e12568c3b73ed0d342097400d75bd919f6
jbergen <jbergen@gmail.com>
2013-01-06 13:21:15 -0500

    formatting

--- app/modules/controls.js
+++ app/modules/controls.js
@@ -27,8 +27,8 @@ function(app, Backbone) {
     },
 
     skip: function() {
-      if ( this.model.status.attributes.current_frame_model.get('_prev') === null && this.model.status.attributes.current_frame != 27708 && this.model.status.attributes.current_frame != 28506 ) {
-          var adv = this.model.status.attributes.current_frame_model.layers.models[0].get('attr').cue_in + this.model.status.attributes.current_frame_model.get('attr').advance / 1000;
+      if ( this.model.status.attributes.current_frame_model.get("_prev") === null && this.model.status.attributes.current_frame != 27708 && this.model.status.attributes.current_frame != 28506 ) {
+          var adv = this.model.status.attributes.current_frame_model.layers.models[0].get("attr").cue_in + this.model.status.attributes.current_frame_model.get("attr").advance / 1000;
           
           this.model.status.attributes.current_frame_model.layers.models[0].visualElement.mediaPlayer.setCurrentTime( adv );
           this.model.cueNext();
