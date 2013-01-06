c34992e5aa6955b956bbae0c64eac7c9731265f3
jbergen <jbergen@gmail.com>
2013-01-06 16:01:03 -0500

    navigation updated

--- app/modules/controls.js
+++ app/modules/controls.js
@@ -22,19 +22,37 @@ function(app, Backbone) {
     },
 
     events: {
-      "click .continue-arrow": "skip",
-      "click .back-arrow": "skipBack"
+      "click .next-arrow": "skip",
+      "click .prev-arrow": "skipBack",
+
+      "mouseenter .prev-arrow" : "mouseenterPrev",
+      "mouseenter .next-arrow" : "mouseenterNext",
+      "mouseout .prev-arrow" : "mouseoutPrev",
+      "mouseout .next-arrow" : "mouseoutNext",
+    },
+
+    mouseenterNext: function() {
+      this.$(".arrow-text").addClass("next");
+    },
+    mouseoutNext: function() {
+      this.$(".arrow-text").removeClass("next");
+    },
+    mouseenterPrev: function() {
+      this.$(".arrow-text").addClass("prev");
+    },
+    mouseoutPrev: function() {
+      this.$(".arrow-text").removeClass("prev");
     },
 
     skip: function() {
       if ( this.model.status.attributes.current_frame_model.get("_prev") === null && this.model.status.attributes.current_frame != 27708 && this.model.status.attributes.current_frame != 28506 ) {
-          var adv = this.model.status.attributes.current_frame_model.layers.models[0].get("attr").cue_in + this.model.status.attributes.current_frame_model.get("attr").advance / 1000;
-          
-          this.model.status.attributes.current_frame_model.layers.models[0].visualElement.mediaPlayer.setCurrentTime( adv );
-          this.model.cueNext();
-        } else {
-          this.model.cueNextSequence();
-        }
+        var adv = this.model.status.attributes.current_frame_model.layers.models[0].get("attr").cue_in + this.model.status.attributes.current_frame_model.get("attr").advance / 1000;
+        
+        this.model.status.attributes.current_frame_model.layers.models[0].visualElement.mediaPlayer.setCurrentTime( adv );
+        this.model.cueNext();
+      } else {
+        this.model.cueNextSequence();
+      }
     },
 
     skipBack: function() {
