9e12c3b6e8e04c787bca7f904ffa72d01cf0bc2f
Joseph Bergen <jbergen@gmail.com>
2013-01-09 16:16:18 -0500

    adding dist files

---
+++ dist/debug/templates.js
@@ -0,0 +1,75 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["app/templates/bgb-end.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<div class=\'END-inner-wrapper\'>\n  <div class=\'headline\'>\n    THANKS FOR WATCHING<br/>ROUGH RIDE\n  </div>\n\n  <div class=\'end-actions\'>\n    <a href=\'http://blackgoldboom.com\' target=\'blank\' class=\'visit-link\'>VISIT THE FULL SITE FOR<br/>MORE STORIES</a>\n    <a class=\'share-icons\' target=\'blank\' href=\'http://twitter.com/share?text=Check%20out%20Black%20Gold%20Boom&url=http://www.blackgoldboom.org/\'><img src=\'assets/img/twitter-large.png\'/></a>\n        \n    <a class=\'share-icons\' target=\'blank\' href=\'http://www.facebook.com/sharer.php?u=http://www.blackgoldboom.com/\'><img src=\'assets/img/facebook-large.png\'/></a>\n    <a class=\'share-icons\' target=\'blank\' href=\'mailto:your-friend@example.com?subject=Black%20Gold%20Boom!&body=http://www.blackgoldboom.com/\'><img src=\'assets/img/email-large.png\'/></a>\n  </div>\n</div>\n<div class=\'END-footer\'>\n  Brought to you by Prairie Public, Zeega, and Localore, a national initiative produced by AIR, with financial support from the Corporation for Public Broadcasting, the Wyncote Foundation, the John D. and Catherine T. MacArthur Foundation, and the National Endowment for the Arts.\n</div>\n<div class="END-bg" style="background: url('+
+( rndImgUrl )+
+'); background-position: 50% 50%; background-repeat: no-repeat no-repeat; background-attachment: fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;"></div>\n';
+}
+return __p;
+};
+
+this["JST"]["app/templates/bgbloader.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<div class="loader-content">\n\t<div class="logo">\n\t\t<div class="logo-splat"></div>\n        <div class="logo-splat-under"></div>\n\t</div>\n\t<div class="intro-text">\n\t\t<p>Rumble. Screech. Bam.</p>\n\t\t<p>For the next 20 minutes or so, you\'ll see and hear stories from North Dakota oil country.</p>\n\t\t<p>This interactive documentary has 9 chapters, each with its own video. Plus there are songs, data and tips from people working there.</p>\n\t\t<p>Let\'s go.</p>\n\t\t<div class=\'fullscreen-loader\'>\n\t\t\tGET THE BEST EXPERIENCE IN\n\t\t\t<a href=\'#\' class=\'fullscreen\'> <img src="assets/img/fullscreen-icon.png" width="12" height="12"> FULL SCREEN</strong>\n\t\t</div>\n\t</div>\n</div>\n<div class="loader-logo-shelf">\n\t<a href="http://www.prairiepublic.org/" target="_blank"><img src="assets/img/logos/logo-prarie-public.png"  width="86" height="66" alt="Prarie Public"></a>\n\t<a href="http://2belowzero.org/" target="_blank"><img src="assets/img/logos/logo-2-below-zero.png" width="71" height="62" alt="2 below zero"></a>\n\t<a href="http://www.airmediaworks.org/" target="_blank"><img src="assets/img/logos/logo-air.png" width="61" height="39" alt="AIR"></a>\n\t<a href="http://airmediaworks.org/localore" target="_blank"><img src="assets/img/logos/logo-localore.png" width="153" height="34" alt="Localore"></a>\n\t<a href="http://zeega.org/" target="_blank"><img src="assets/img/logos/logo-zeega.png" width="101" height="31" alt="Zeega"></a>\n</div>\n<div class="ZEEGA-loader-bg" style="background: url('+
+( rndImgUrl )+
+'); background-position: 50% 50%; background-repeat: no-repeat no-repeat; background-attachment: fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;"></div>';
+}
+return __p;
+};
+
+this["JST"]["app/templates/controls.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<div class="nav-box">\n    <a class=\'prev-arrow\'></a>\n    <a class=\'next-arrow\'></a>\n    <div class=\'arrow-text\'></div>\n</div>';
+}
+return __p;
+};
+
+this["JST"]["app/templates/share.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='  <div class=\'overlay-wrapper\'>\n    <div class=\'BGB-overlay-title\'><h2>SHARE BLACK GOLD BOOM\'S ROUGH RIDE</h2></div>\n    <a href=\'#\' class=\'close-modal\'><h3>close</h3></a>\n\n\n    <div class=\'BGB-overlay-content-wrapper-plain clearfix\'>\n\n      <a class=\'share-icons\' target=\'blank\' href=\'http://twitter.com/share?text=Check%20out%20Black%20Gold%20Boom&url=http://roughride.blackgoldboom.com/\'><img src=\'assets/img/twitter-large.png\'/></a>\n        \n      <a class=\'share-icons\' target=\'blank\' href=\'http://www.facebook.com/sharer.php?u=http://roughride.blackgoldboom.com/\'><img src=\'assets/img/facebook-large.png\'/></a>\n      <a class=\'share-icons\' target=\'blank\' href=\'mailto:your-friend@example.com?subject=Black%20Gold%20Boom!&body=http://roughride.blackgoldboom.com/\'><img src=\'assets/img/email-large.png\'/></a>\n      <br/>\n\n    </div>\n  </div>';
+}
+return __p;
+};
+
+this["JST"]["app/templates/titles.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<a href=\'#\' class=\'play-pause\'></a>\n<div class=\'title-bar-above\'>\n  <div class=\'title-level\'>\n    <h1>'+
+( title )+
+'</h1>\n    <div class=\'elapsed\'>\n      <span class=\'time-elapsed\'>1:23</span> / <span class=\'time-duration\'>4:56</span>\n    </div>\n  </div>\n  <h2>'+
+( seqTitle )+
+'</h2>\n  <div class=\'aux-controls\'>\n    <div class=\'\'><a href=\'#\' class=\'share\'>SHARE</a></div>\n    <div class=\'\'><a href=\'#\' class=\'fullscreen\'>FULLSCREEN</a></div>\n  </div>\n</div>\n<div class=\'elapsed-bar-wrapper\'>\n  <div class=\'elapsed-bar\'></div>\n</div>';
+}
+return __p;
+};
+
+this["JST"]["app/templates/ui-base.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<a href="http://blackgoldboom.com/" target="_blank" class="bgb-logo" title="View more stories on blackgoldboom.com">\n    <img src="assets/img/bgblogo.png" width="85" height="92" alt="Black Gold Boom">\n</a>\n<div class="block-clickthrough"></div>\n<span class="bottom-credit">powered by <a href="http://zeega.org" target="_blank">zeega</a></span>';
+}
+return __p;
+};
+
+this["JST"]["app/templates/zeega-popup.html"] = function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='<h1 class=\'popup-title\'>'+
+( name )+
+'</h1>\n<div id="ZEEGA-popup-'+
+( id )+
+'" class="ZEEGA-popup-project-target"></div>\n';
+ if ( caption != "" ) { 
+;__p+='\n  <div class=\'caption\'>\n    '+
+( caption )+
+'\n    <div class=\'actions\'>\n      <a href="#" class="continue">Ok, let me see</a>\n      <a href="#" class="skip">Skip the gallery</a>\n    </div>\n  </div>\n';
+ } 
+;__p+='';
+}
+return __p;
+};
